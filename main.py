d2e9759dde94bc692bd981d0d694d94c8f700fdb
Anreet Kaur <anreet.kaur@sheridancollege.ca>
2024-02-19 13:55:22 -0500

    [anreet] modified main.py

--- main.py
+++ main.py
@@ -71,51 +71,6 @@ def showMap():
     coordinates = collect_coordinates_bus_shelter('staticFiles/Transit-Shelter-Data.csv')
     return render_template('map.html', coordinates=coordinates, source_loc = userCoordinates[0], dest_loc = userCoordinates[1], API_KEY=os.environ.get('GOOGLE_MAPS_API_KEY'))
 
-     
-
-# @app.route('/')
-# def map_markers():
-#     # Collect coordinates from the bus shelter dataset
-#     coordinates = extractDataFromDatabase()
-#     return render_template('map.html', coordinates=coordinates, API_KEY=os.environ.get('GOOGLE_MAPS_API_KEY'))
-
-# @app.route('/calculate_route', methods=['POST'])
-# def calculate_route():
-#     data = request.json
-#     origin = data.get('origin', '43.7729984,-79.5084078')  #(York Keele Campus) Extract origin coordinates from request data
-#     destination = data.get('destination', '43.6621761,-79.3984965')  # (Uoft) Extract destination coordinates from request data
-#     api_key = os.environ.get('GOOGLE_MAPS_API_KEY')
-
-#     if origin is None or destination is None:
-#         return jsonify({'error': 'Please provide both origin and destination coordinates.'}), 400
-
-#     if api_key is None:
-#         return jsonify({'error': 'Google Maps API key not found.'}), 500
-
-#     # Make a request to Google Maps Directions API
-#     url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={api_key}"
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         route_data = response.json()
-#         return jsonify(route_data)
-#     else:
-#         return jsonify({'error': 'Failed to fetch route data.'}), 500
-     
-# @app.route('/api/submit-form', methods=['GET']) 
-# def submit_form():
-#     return render_template('demo.html')
-
-# @app.route('/api/submit-form', methods=['POST'])
-# def receive_data(): 
-#     source_address = request.form.get('source_address')
-#     destination_address = request.form.get('destination_address')
-#     if source_address and destination_address:
-#         print("Recieved Data:", source_address, destination_address)
-#         return jsonify({'status': 'success', 'message': 'Data received successfully!'})
-#     else:
-#         return jsonify({'status': 'error', 'message': 'Invalid Data Received!'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
